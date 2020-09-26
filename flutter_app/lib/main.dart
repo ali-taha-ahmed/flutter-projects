9c418f5e489219034832ca5af667ae93f45d9692
ALI TAHA <ali.taha.projects@gmail.com>
2020-09-26 17:45:13 +0530

    first commit

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "im poor app",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Im poor app"),
+          backgroundColor: Colors.tealAccent,
+        ),
+        body: Center(
+          child: Image.asset("images/charcoal.png"),
+        ),
+      ),
+    );
+
+  }
+}
