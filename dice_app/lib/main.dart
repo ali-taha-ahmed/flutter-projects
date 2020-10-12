b207e29af348b2c8c4bdb202b470ada1456eba08
ALI TAHA <ali.taha.projects@gmail.com>
2020-10-12 08:26:51 +0530

    completed the dice app

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -10,40 +11,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Dice App',
         home: Scaffold(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.tealAccent.shade700,
           appBar: AppBar(
-            backgroundColor: Colors.red,
-            title: Center(
-              child: Text("Dice App"),
-            ),
+            backgroundColor: Colors.tealAccent.shade400,
+            title: Center(child: Text("Dice App")),
           ),
           body: DicePage(),
         ));
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 1;
-    var rightDiceNumber = 1;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: FlatButton(
               child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: () {},
+              onPressed: () {
+                rollDice();
+              },
             ),
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-              onPressed: () {},
-            ),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  rollDice();
+                }),
           ),
         ],
       ),
     );
+  }
+
+  int rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
